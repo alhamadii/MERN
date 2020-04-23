f046fe91b656139119440f186fe362c820fa5a4f
Hamad Alhammadi <bin.was@gmail.com>
2020-04-23 23:06:36 +0300

    Prepared for deployment

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -7,13 +7,13 @@ import { login } from "../../actions/auth";
 const Login = ({ login, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     email: "",
-    password: ""
+    password: "",
   });
   const { email, password } = formData;
-  const onchange = e =>
+  const onchange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const onSubmit = async e => {
+  const onSubmit = async (e) => {
     e.preventDefault();
 
     login(email, password);
@@ -29,14 +29,14 @@ const Login = ({ login, isAuthenticated }) => {
       <p className='lead'>
         <i className='fas fa-user'></i> Sign Into Your Account
       </p>
-      <form className='form' onSubmit={e => onSubmit(e)}>
+      <form className='form' onSubmit={(e) => onSubmit(e)}>
         <div className='form-group'>
           <input
             type='email'
             placeholder='Email Address'
             name='email'
             value={email}
-            onChange={e => onchange(e)}
+            onChange={(e) => onchange(e)}
             required
           />
         </div>
@@ -46,12 +46,14 @@ const Login = ({ login, isAuthenticated }) => {
             placeholder='Password'
             name='password'
             value={password}
-            onChange={e => onchange(e)}
+            onChange={(e) => onchange(e)}
             minLength='6'
           />
         </div>
-
-        <input type='submit' className='btn btn-primary' value='Login' />
+        <input type='submit' className='btn btn-primary' value='Login' />{" "}
+        <p className='my-1'>
+          Forget Password <Link to='#!'>Reset</Link>
+        </p>
       </form>
       <p className='my-1'>
         Don't have an account? <Link to='/register'>Sign Up</Link>
@@ -62,10 +64,10 @@ const Login = ({ login, isAuthenticated }) => {
 
 login.prototype = {
   login: PropTypes.func.isRequired,
-  isAuthenticated: PropTypes.bool
+  isAuthenticated: PropTypes.bool,
 };
 
-const mapStateToProps = state => ({
-  isAuthenticated: state.auth.isAuthenticated
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
 });
 export default connect(mapStateToProps, { login })(Login);
